0e114cafca465526d5b73b828adbb047629dd166
nspmultiserve <56471390+nsptekweb@users.noreply.github.com>
2019-11-21 19:38:32 +0530

    old files

---
+++ nav_old.php
@@ -0,0 +1,105 @@
+<header id="header-part">
+
+    <div class="header-top d-none d-lg-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="header-contact text-lg-left text-center">
+                        <ul>
+                            <li><img src="images/all-icon/map.png" alt="icon"><span>127/5 Mark street, New york</span></li>
+                            <li><img src="images/all-icon/email.png" alt="icon"><span>info@yourmail.com</span></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="header-opening-time text-lg-right text-center">
+                        <p>Opening Hours : Monday to Saturay - 8 Am to 5 Pm</p>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- header top -->
+
+    <div class="header-logo-support pt-30 pb-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4">
+                    <div class="logo">
+                        <a href="index.php">
+                            <img src="images/128.png" alt="Logo" class="logo-img">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-8">
+                    <div class="support-button float-right d-none d-md-block">
+                        <div class="support float-left">
+                            <div class="icon">
+                                <img src="images/all-icon/support.png" alt="icon">
+                            </div>
+                            <div class="cont">
+                                <p>Need Help? call us free</p>
+                                <span>321 325 5678</span>
+                            </div>
+                        </div>
+                        <div class="button float-left">
+                            <a href="#" class="main-btn">Apply Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- header logo support -->
+
+    <div class="navigation" style=" background-color: #ef4723;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-9 col-8">
+                    <nav class="navbar navbar-expand-lg">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse sub-menu-bar" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item">
+                                    <a href="index.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="about.php">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#">Courses</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Courses 1</a></li>
+                                        <li><a href="#">Courses 2</a></li>
+                                        <li><a href="#">Courses 3</a></li>
+                                        <li><a href="#">Courses 4</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#"></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="testimonials.php">Testimonials</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="image-gallery.php">Gallery</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="blog.php">Blog</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="contact.php">Contact Us</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav> <!-- nav -->
+                </div>
+
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div>
+
+</header>
