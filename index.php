5622f4521a46205f147e1e77628682474e00b6e7
nspmultiserve <56471390+nsptekweb@users.noreply.github.com>
2019-11-21 19:47:18 +0530

    index

--- index.php
+++ index.php
@@ -134,7 +134,7 @@
                     <div class="col-lg-5">
                         <div class="section-title mt-50">
                        
-                            <h2>Welcome to Edubin </h2>
+                            <h2>Welcome to German Akademie </h2>
                         </div> <!-- section title -->
                         <div class="about-cont">
                             <p>Lorem ipsum gravida nibh vel velit auctor aliquetn sollicitudirem quibibendum auci elit cons equat ipsutis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet . Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt  mauris. <br> <br> auci elit cons equat ipsutis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet . Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt  mauris</p>
@@ -144,7 +144,7 @@
                     <div class="col-lg-6 offset-lg-1">
                         <div class="about-event mt-50">
                             <div class="event-title">
-                                <h3>Upcoming events</h3>
+                                <h3>Upcoming Batches</h3>
                             </div> <!-- event title -->
                             <ul>
                                 <li>
